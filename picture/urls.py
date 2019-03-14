2312637b863a02c4e89c299a2bc3f060a7e40aa3
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-03-14 17:04:24 +0200

    first commit

---
+++ picture/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    url('^$',views.index, name = 'home'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_image, name='art'),
+    url(r'^location/(?P<location>\d+)', views.location_filter, name='location_filter'),
+    url(r'^search', views.search, name='search_images')
+]
+
