98442c24e75c9273a15c20badfdc3458569d0588
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-03-15 12:28:42 +0200

    add search.html

--- picture/urls.py
+++ picture/urls.py
@@ -9,5 +9,5 @@ urlpatterns=[
     url(r'^image/(\d+)',views.image,name ='image'),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
