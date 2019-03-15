865a185d3ede22cda4e94497743722069aa97030
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-03-15 10:24:32 +0200

    add models.py

--- garely/settings.py
+++ garely/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'picture.apps.PictureConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'picture.apps.PictureConfig',
-    'bootstrap3',
+   
 ]
 
 MIDDLEWARE = [
